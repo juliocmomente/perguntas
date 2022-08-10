af26de9c9b1655a4b9578c7de8661c3ebfc0724c
Julio Momente <juliocesar.momente@gmail.com>
2022-08-09 21:23:37 -0300

    Incluindo mais widgets
    Incluindo o widget de column e children

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,32 @@ main() {
 class PerguntasApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual a sua cor favorita?',
+      'Qual o seu animal favorito?'
+    ];
+
     return MaterialApp(
-      home: Text('Olá Flutter!!!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas.elementAt(0)),
+            ElevatedButton(
+                onPressed: responder, child: const Text('Resposta 1')),
+            ElevatedButton(
+                onPressed: responder, child: const Text('Resposta 2')),
+            ElevatedButton(
+                onPressed: responder, child: const Text('Resposta 3')),
+          ],
+        ),
+      ),
     );
+  }
+
+  void responder() {
+    print('Resposta!');
   }
 }
